bf036477295b55b5679ee7386c8dad245c18ea3d
inovue <inovue3@gmail.com>
2024-06-25 18:35:41 +0900

    chore: Update installation script to include package installation from apt-packages.txt

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# dotfiles
+
 for i in "$(ls -d */)"; do 
   stow "$i"
 done
+
+
+# apt 
+
+PACKAGE_LIST="apt-packages.txt"
+
+if [[ ! -f $PACKAGE_LIST ]]; then
+  echo "$PACKAGE_LIST not found!"
+  exit 1
+fi
+
+while IFS= read -r package; do
+  if ! dpkg -l | grep -q "^ii  $package"; then
+    sudo apt install -y $package
+  else
+    echo "$package is already installed."
+  fi
+done < "$PACKAGE_LIST"
