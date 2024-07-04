ffa57c2c316cc785bef9d976a1698545abab7c68
inovue <inovue3@gmail.com>
2024-07-04 11:21:17 +0900

    chore: add xsel

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt update && sudo apt upgrade
 
 # apt packages
-sudo apt -y install zsh stow curl
+sudo apt -y install zsh stow curl xsel
 
 ## python require
 sudo apt -y install build-essential libbz2-dev libdb-dev \
@@ -14,34 +14,28 @@ sudo apt -y install build-essential libbz2-dev libdb-dev \
 ## ruby require
 sudo apt -y install libyaml-dev unzip
 
-
 # stow
 for dir in */; do
   stow "${dir%/}"
 done
 
-
 # nvim
 curl -LO https://github.com/neovim/neovim-releases/releases/download/v0.10.0/nvim-linux64.deb && sudo apt install ./nvim-linux64.deb && rm -f ./nvim-linux64.deb
 
-
-# mise 
+# mise
 [ ! -f "~/.local/bin/mise" ] && curl https://mise.run | sh
 mise install -y
 
-
-# gh (github cli) 
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
-&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
-
+# gh (github cli)
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) &&
+  sudo mkdir -p -m 755 /etc/apt/keyrings &&
+  wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
+  sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+  sudo apt update &&
+  sudo apt install gh -y
 
 # colorls
 gem install colorls
-
 
 chsh -s $(which zsh)
