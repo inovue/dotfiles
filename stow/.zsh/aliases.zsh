01f2379880f4570463715b196cac73c1711307d1
inovue <inovue3@gmail.com>
2025-01-25 22:06:56 +0900

    Update Zsh aliases with new Git and repository management shortcuts
    - Add convenient aliases for navigating and managing GitHub repositories using ghq and fzf
    - Modify existing `l` and `ll` aliases to improve default output of lsd
    - Include additional Git-related aliases for cleaning and managing repositories

--- stow/.zsh/aliases.zsh
+++ stow/.zsh/aliases.zsh
@@ -1,4 +1,9 @@
-alias l='lsd --date "+%Y-%m-%d %H:%M:%S (%a)"'
-alias ll='ls -la'
+alias l='lsd'
+alias ll='lsd -la --date "+%Y-%m-%d %H:%M:%S (%a)"'
 
 alias clear='source $HOME/.zshrc && clear'
+
+alias ghcd='cd $(ghq root)/$(ghq list | fzf)'
+alias ghrm='ghq rm $(ghq list | fzf)'
+alias ghcp='repo=$(gh repo list --json name,url --jq "sort_by(.name) | .[].url" | fzf) && ghq get $repo && cd $(ghq list -p -e $repo)'
+alias gclean='git clean -fdx && test $(git ls-files | wc -l) -eq 0 || git rm -rf .'
