bc16ac176ff9ed4d0a987ceec79bc899e79e9da1
inovue <inovue3@gmail.com>
2025-02-27 10:30:39 +0900

    Add Dockge and new Zsh alias
    - Import Dockge installation task in Ansible playbook
    - Add convenient Dockge web interface alias in Zsh configuration

--- stow/.zsh/aliases.zsh
+++ stow/.zsh/aliases.zsh
@@ -7,3 +7,5 @@ alias ghcd='cd $(ghq root)/$(ghq list | fzf)'
 alias ghrm='ghq rm $(ghq list | fzf)'
 alias ghcp='repo=$(gh repo list --json name,url --jq "sort_by(.name) | .[].url" | fzf) && ghq get $repo && cd $(ghq list -p -e $repo)'
 alias gclean='git clean -fdx && test $(git ls-files | wc -l) -eq 0 || git rm -rf .'
+
+alias dockge='wslview http://localhost:5001'
