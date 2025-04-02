80e3307b8b70dac707ceb567f317271a6eb1c545
inovue <inovue3@gmail.com>
2025-04-02 13:52:04 +0900

    Add new Zsh alias for remote repository deletion
    - Introduce alias 'ghrm-remote' to delete a GitHub repository using the GitHub CLI
    - Enhance workflow for managing remote repositories

--- stow/.zsh/aliases.zsh
+++ stow/.zsh/aliases.zsh
@@ -6,6 +6,7 @@ alias clear='source $HOME/.zshrc && clear'
 alias ghcd='cd $(ghq root)/$(ghq list | fzf)'
 alias ghrm='ghq rm $(ghq list | fzf)'
 alias ghcp='repo=$(gh repo list --json name,url --jq "sort_by(.name) | .[].url" | fzf) && ghq get $repo && cd $(ghq list -p -e $repo)'
+alias ghrm-remote='repo=$(gh repo list --json name,url --jq "sort_by(.name) | .[].url" | fzf) && gh repo delete $repo --yes'
 alias gclean='git clean -fdx && test $(git ls-files | wc -l) -eq 0 || git rm -rf .'
 
 alias dockge='wslview http://localhost:5001'
